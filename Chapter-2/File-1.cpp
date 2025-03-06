7a54784511f569193c9cd2d49a8bbd152c84dd06
mdkawsarmiah <mdkawsarmiah33@gmail.com>
2025-03-06 23:00:58 +0600

    Insertion at Beginning

--- Chapter-2/File-1.cpp
+++ Chapter-2/File-1.cpp
@@ -5,18 +5,29 @@ using namespace std;
 
 int main(){
 int n;
-cout << "Enter your array size: ";
+cout << "\nEnter your array size: ";
 cin >> n;
 
 int array[n];
 
-cout << "Enter your elements: " << endl;
+cout << "\nEnter your Elements: " << endl;
 
 for(int i = 0; i< n ; i++){
     cout << "Elements [" << i +1 << "] : " ;
     cin >> array[i];
 }
 
+int x;
+cout << "\nEnter Elements at beganning: ";
+cin >> x;
+
+for(int i =n; i>0;i--){
+    array[i] = array [i-1];
+}
+for(int i = 0; i < n; i++){
+array[x];
+n++;
+}
 cout << "\nYour Elements: " << endl;
 for(int i = 0; i< n ; i++){
     cout << array[i] << ", ";
