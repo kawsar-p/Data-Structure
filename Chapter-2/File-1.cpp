5d237a2c2f389079831baba235472998ab42f34e
Md. Kawsar Miah <mdkawsarmiah33@gmail.com>
2025-03-06 16:13:13 +0000

    C-1

--- Chapter-2/File-1.cpp
+++ Chapter-2/File-1.cpp
@@ -1,1 +1,26 @@
+// Insertation in the first in array
 
+#include<iostream>
+using namespace std;
+
+int main(){
+int n;
+cout << "Enter your array size: ";
+cin >> n;
+
+int array[n];
+
+cout << "Enter your elements: " << endl;
+
+for(int i = 0; i< n ; i++){
+    cout << "Elements [" << i +1 << "] : " ;
+    cin >> array[i];
+}
+
+cout << "\nYour Elements: " << endl;
+for(int i = 0; i< n ; i++){
+    cout << array[i] << ", ";
+}
+
+return 0;
+}
