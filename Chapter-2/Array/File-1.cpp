a731e4b5b5a41d69e75a677b42dc4f2a77a68aa9
Md. Kawsar Miah <mdkawsarmiah33@gmail.com>
2025-03-07 02:35:43 +0600

    Insert at first index

--- Chapter-2/Array/File-1.cpp
+++ Chapter-2/Array/File-1.cpp
@@ -1,37 +1,37 @@
-// Insertation in the first in array
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-int n;
-cout << "\nEnter your array size: ";
-cin >> n;
+int main() {
+    int n;
+    cout << "\nEnter your array size: ";
+    cin >> n;
 
-int array[n];
+    int array[n + 1]; // Increased size to accommodate the new element
 
-cout << "\nEnter your Elements: " << endl;
+    cout << "\nEnter your Elements: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Element [" << i + 1 << "]: ";
+        cin >> array[i];
+    }
 
-for(int i = 0; i< n ; i++){
-    cout << "Elements [" << i +1 << "] : " ;
-    cin >> array[i];
-}
+    int x;
+    cout << "\nEnter element to insert at the beginning: ";
+    cin >> x;
 
-int x;
-cout << "\nEnter Elements at beganning: ";
-cin >> x;
+    // Shift elements to the right
+    for (int i = n; i > 0; i--) {
+        array[i] = array[i - 1];
+    }
+    
+    // Insert the new element at the beginning
+    array[0] = x;
+    n++; // Increase the array size counter
 
-for(int i =n; i>0;i--){
-    array[i] = array [i-1];
-}
-for(int i = 0; i < n; i++){
-array[x];
-n++;
-}
-cout << "\nYour Elements: " << endl;
-for(int i = 0; i< n ; i++){
-    cout << array[i] << ", ";
-}
-
-return 0;
+    cout << "\nYour Elements: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
 }
